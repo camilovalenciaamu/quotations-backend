a0b349d991fcb43346415b635087a75739cc38e1
Camilo valencia amú <camiloamu82@gmail.com>
2021-04-25 15:04:50 -0500

    fix: Se corrije en la tabla html el numero de telefono

--- app/Http/Controllers/Quotation/QuotationController.php
+++ app/Http/Controllers/Quotation/QuotationController.php
@@ -12,15 +12,16 @@ class QuotationController extends Controller
 
     public function CreateQuotation(QuotationRequest $request, Quotation $quotation, QuotationService $service, SendEmailService $email)
     {
-        $diff = $service->GetDiffInHours($request->email);
-        if ($diff['diff'] < 24 && $diff['message'] == 'esta')
-            return response()->json(['error' => "¡Ya tienes una cotización, debes esperar 24 horas para crear otra!"], 400);
+        // $diff = $service->GetDiffInHours($request->email);
+        // if ($diff['diff'] < 24 && $diff['message'] == 'esta')
+        //     return response()->json(['error' => "¡Ya tienes una cotización, debes esperar 24 horas para crear otra!"], 400);
         $emails = [
-            'nleon@processoft.com.co','ocalero@processoft.com.co','jmartinez@processoft.com.co'
+            // 'nleon@processoft.com.co','ocalero@processoft.com.co','jmartinez@processoft.com.co'
+            'camiloamu82@gmail.com'
         ];
 
-        $email->SendEmail($emails, $request);
-        return response()->json(['cotizacion' => $quotation->create($request->validated())], 200);
+        return $email->SendEmail($emails, $request);
+        // return response()->json(['cotizacion' => $quotation->create($request->validated())], 200);
     }
 
 }
